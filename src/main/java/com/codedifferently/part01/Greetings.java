c0118a377a3a60be109d1d7d48c5d3061a4fed8c
victor840 <victor.ugoanusi@gmail.com>
2020-10-24 20:41:37 +0000

    initial commit

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,28 @@
 package com.codedifferently.part01;
 
-public class Greetings {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Greetings {
+    public static void main(String[] args){
+        //we created a scanner object to collect input from user.
+          Scanner scanner = new Scanner(System.in);
+        //we used system out println to give feedback to the user
+          System.out.println("What's your name?: ");
+          String input = scanner.next();
+          System.out.println("Seems like you entered your name as " +input);
+        //TO DO if the user's name is Hakim or Rasheeda, say "hello"
+         if (input.equalsIgnoreCase("Hakim") || input.equalsIgnoreCase("Rasheeda")
+            ){ 
+              System.out.println("Why hello there! I know you! Welcome Back! :-) ");
+             }
+        //TO DO if the user's name is not Hakim or Rasheeda, say "IDK yuh Bruv"
+             else {
+                 System.out.println("It was nice to meet you, but IDK yuh Bruv. Buh-Bye!!!");
+             }
+
+       
+        
     }
+
+  
 }
